16a34b02749f5dea225e93c92d3b14f1806f42b2
Stephanie Li <41097137+baojiao9799@users.noreply.github.com>
2022-11-04 13:26:58 -0400

    THOS-10: Pass through splitOn in Dapper Service (#10)
    * pass through splitOn
    
    * bump version

--- DataOnion/db/IDapperService.cs
+++ DataOnion/db/IDapperService.cs
@@ -13,42 +13,42 @@ namespace DataOnion.db
         Task<IEnumerable<TFirst>> QueryAndReturnAsync<TFirst, TSecond>(
             string query,
             Func<TFirst, TSecond, TFirst> mappingFunction,
-            string? splitOn = null, 
+            string splitOn = "Id", 
             object? parameters = null
         );
 
         Task<IEnumerable<TFirst>> QueryAndReturnAsync<TFirst, TSecond, TThird>(
             string query,
             Func<TFirst, TSecond, TThird, TFirst> mappingFunction,
-            string? splitOn = null,
+            string splitOn = "Id",
             object? parameters = null
         );
 
         Task<IEnumerable<TFirst>> QueryAndReturnAsync<TFirst, TSecond, TThird, TFourth>(
             string query,
             Func<TFirst, TSecond, TThird, TFourth, TFirst> mappingFunction,
-            string? splitOn = null,
+            string splitOn = "Id",
             object? parameters = null
         );
 
         Task<IEnumerable<TFirst>> QueryAndReturnAsync<TFirst, TSecond, TThird, TFourth, TFifth>(
             string query,
             Func<TFirst, TSecond, TThird, TFourth, TFifth, TFirst> mappingFunction,
-            string? splitOn = null,
+            string splitOn = "Id",
             object? parameters = null
         );
 
         Task<IEnumerable<TFirst>> QueryAndReturnAsync<TFirst, TSecond, TThird, TFourth, TFifth, TSixth>(
             string query,
             Func<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TFirst> mappingFunction,
-            string? splitOn = null,
+            string splitOn = "Id",
             object? parameters = null
         );
 
         Task<IEnumerable<TFirst>> QueryAndReturnAsync<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TSeventh>(
             string query,
             Func<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TSeventh, TFirst> mappingFunction,
-            string? splitOn = null,
+            string splitOn = "Id",
             object? parameters = null
         );
 
